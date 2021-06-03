40a5362e1d8e36f57dfb585612b5c329cbe85f67
sunjus <sunju.shin1108@gmail.com>
2021-06-03 18:37:51 +0300

    Updated landing page

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,36 +1,67 @@
 import React from "react";
 import styled from "styled-components";
-import hero from "../images/hero.png";
+import react from "../images/reactlogo.png";
+import typescript from "../images/typescriptlogo.png";
+import sass from "../images/sasslogo.png";
+import jest from "../images/jestlogo.png";
+import node from "../images/nodelogo.png";
+import express from "../images/expresslogo.png";
+import mongodb from "../images/mongodblogo.jpg";
 
 function HomePage() {
   return (
     <HomePageStyled>
-      <div>
-        <img src={hero} alt="tech" />
-      </div>
       <div className="typography">
         <h2>
           Hi, I'm <span>Sunju</span>, a Front-end developer.
         </h2>
         <h3>
-          I build interactive, intuitive, responsive and scalable web-based
-          applications using modern web-technologies and tools
+          I build interactive, intuitive and responsive web-based applications
+          using modern web-technologies and tools.
         </h3>
-        <h3>from the front-end to the backend.</h3>
+        <h3>
+          Through consistent daily practice, I’ve managed to develop a skill set
+          that covers both ends of the spectrum, from the front-end to the
+          backend. Currently, my stronger side is front-end development.
+        </h3>
       </div>
       <div className="stack">
-        <h2 class="ltitle">Top Technologies</h2>
-        <div class="hoby row no-margin">
-          <p>
-            <i class="fab fa-js-square"></i>Javascript <span> </span>
-            <i class="fab fa-react"></i>React <span> </span>
-            <i class="fab fa-js-square"></i>Typescript <span> </span>
-          </p>
-          <p>
-            <i class="fab fa-node"></i> Nodejs <span> </span>
-            <i class="fab fa-node"></i> Express <span> </span>
-          </p>
-          <i class="fas fa-database"></i>MongoDB
+        <h2 className="ltitle">Top Technologies</h2>
+        <div className="tech">
+          <div className="tech-front">
+            <h3>Front-end development</h3>
+            <div className="tech-icon">
+              <img src={react} />
+              <p> React.js</p>
+            </div>
+            <div className="tech-icon">
+              <img src={typescript} />
+              <p>Typescript</p>
+            </div>
+            <div className="tech-icon">
+              <img src={sass} />
+              <p>Sass</p>
+            </div>
+            <div className="tech-icon">
+              <img src={jest} />
+              <p>Jest</p>
+            </div>
+          </div>
+          <div className="tech-server">
+            <h3>Server-side development</h3>
+            <div className="tech-icon">
+              <img src={node} />
+              <p>Node.js</p>
+            </div>
+            <div className="tech-icon">
+              <img src={express} />
+              <p>Express.js</p>
+            </div>
+            <div className="tech-icon">
+              <img src={mongodb} />
+              <p>MongoDB</p>
+            </div>
+          </div>
         </div>
       </div>
     </HomePageStyled>
@@ -38,39 +69,35 @@ function HomePage() {
 }
 
 const HomePageStyled = styled.header`
+  display: block;
   width: 100%;
   height: 100vh;
-  position: relative;
-  background: linear-gradient(to bottom, #f08c89, bisque);
-
-  .typography {
-    position: absolute;
-    top: 45%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    text-align: center;
-    width: 80%;
-  }
-
+  padding: 5rem;
+  text-align: center;
   .stack {
-    position: absolute;
-    top: 60%;
-    left: 50%;
-    transform: translate(-50%, -50%);
     text-align: center;
-    width: 80%;
-    .ltitle {
-      color: var(--font-dark-color);
-    }
-
-    .hoby row no-margin {
-      display: flex;
-      .fab .fas {
-        padding: 1rem;
-        margin: 1rem;
-        color: #f08c89;
-      }
-    }
+    padding: 3rem;
+  }
+  .tech {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    padding: 3rem;
+    max-width: 105rem;
+    background-color: bisque;
+  }
+  .tech-frontend .tech-server {
+    display: flex;
+    align-items: center;
+    padding: 4rem 3rem 2rem;
+    max-width: 38rem;
+    background-color: #f8f8f8;
+    border-radius: 0.5rem;
+    box-shadow: -0.5rem -0.5rem -0.5rem #fff;
+    margin: 2rem 1.5rem;
+  }
+  .tech-icon {
+    padding: 0.5rem;
   }
 `;
 
